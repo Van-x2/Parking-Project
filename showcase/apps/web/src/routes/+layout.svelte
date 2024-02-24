cf1556699320fbee9051eba0d0e542bd9f0b8a0e
Vann <111390478+Van-x2@users.noreply.github.com>
2024-02-24 12:11:29 +0000

    Big Changes, Setup basic NavBar and Parkinglot map

---
+++ showcase/apps/web/src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<script>import "../app.pcss";</script><slot></slot>
+
+<div class="navbar">
+  <a class="title" href="/">KSM Parking</a>
+  <div class="navbtns">
+      <a href="/map" class="options">Options</a>
+      <a href="/user" class="user">Current User</a>
+  </div>
+</div>
+
+<style>
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #3d3c52;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  padding: 0.7em;
+  z-index:1000 ;
+}
+
+.title{
+  color: rgb(153, 181, 197);
+  font-size: 2em;
+  font-family: 'Georgia';
+  text-decoration: none;
+}
+
+.title:hover {
+  color: aquamarine;
+}
+
+.navbtns {
+  display: flex;
+  gap: 2em; /* adjust as needed */
+}
+
+.navbtns a {
+  color: #f2f2f2;
+  text-decoration: none;
+  font-size: 20px;
+  padding: 10px;
+  border-radius: 20px;
+}
+
+.navbtns a:hover {
+  background-color: #ddd;
+  color: black;
+}
+</style>
---
+++ showcase/apps/web/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import ParkingLots from "/workspaces/Parking-Project/showcase/apps/web/src/lib/assets/ParkingLots.png"
+</script>
+<slot />
+
