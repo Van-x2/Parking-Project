7a3b00a9eea9e1c41706a2788034c95151dfb693
Vann <111390478+Van-x2@users.noreply.github.com>
2024-03-14 18:21:15 +0000

    More simple updates

--- OriginalProj/showcase/apps/web/src/routes/+page.svelte
+++ OriginalProj/showcase/apps/web/src/routes/+page.svelte
@@ -7,7 +7,7 @@
     <title>KSM Parking</title>
 
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
-  
+
   <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
 
   <script src="https://cdn.jsdelivr.net/gh/pocketbase/js-sdk@latest/dist/pocketbase.umd.js"></script>
@@ -52,9 +52,6 @@
   
 
   <body>
-
-
-  
   <div id="map" style="width: 100%; height: 100%;"></div>
 
 
